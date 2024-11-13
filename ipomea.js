7cec2828ea7f6ef4a7600170157560b4ed0481f3
sebabert143 <sebastianbertoluzza@gmail.com>
2024-11-13 20:23:42 -0300

    oki

--- ipomea.js
+++ ipomea.js
@@ -4,7 +4,7 @@ fetch('footer.html')
     document.getElementById('footer-placeholder').innerHTML = data; 
 });
 
-fetch('nav.html') 
+fetch('navbar.html') 
 .then(response => response.text()) 
 .then(data => { 
     document.getElementById('navbar-placeholder').innerHTML = data; 
