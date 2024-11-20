ed39c210a3765e63739cf998f12d79ae94b797b2
sebabert143 <sebastianbertoluzza@gmail.com>
2024-11-19 21:02:56 -0300

    Update scriptcontacto.js

--- scriptcontacto.js
+++ scriptcontacto.js
@@ -1,15 +1,15 @@
-/* https://magicloops.dev/es */
-function enviarMail(email, nombre, apellido, mensaje) {
-    // Por si necesito usar letrs o simbolos raros
-    const emailEncoded = encodeURIComponent(email);
+function enviarMail(nombre, apellido, mensaje) {
+    // correo que recibe los mensajes:
+    const emailFijo = "sebastianbertoluzza@gmail.com"; 
+    const emailEncoded = encodeURIComponent(emailFijo);
     const nombreEncoded = encodeURIComponent(nombre);
     const apellidoEncoded = encodeURIComponent(apellido);
     const mensajeEncoded = encodeURIComponent(mensaje);
- //Ahora necesitamos la url de la api junto a los valores codificados
- //La api la cree en magicloops.dev y envia el mail a mi casilla personal.
-    const url = `https://magicloops.dev/api/loop/run/101d7b77-63ed-42a9-9dd1-30fa361be960?email=${emailEncoded}&nombre=${nombreEncoded}&apellido=${apellidoEncoded}&mensaje=${mensajeEncoded}`;
+
     
-    fetch(url)//Envia una peticion a la api
+    const url = `https://magicloops.dev/api/loop/run/101d7b77-63ed-42a9-9dd1-30fa361be960?email=${emailEncoded}&nombre=${nombreEncoded}&apellido=${apellidoEncoded}&mensaje=${mensajeEncoded}`;
+
+    fetch(url) 
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error en la solicitud: ' + response.status);
@@ -17,21 +17,21 @@ function enviarMail(email, nombre, apellido, mensaje) {
             return response.json();
         })
         .then(data => {
-            console.log('Respuesta del servidor:', data); // Esperemos que sea un mensaje de ok y estatus 200 jaja
+            console.log('Respuesta del servidor:', data); 
             return true;
         })
         .catch(error => {
             console.error('Hubo un error:', error);
-            return 0;
+            return false;
         });
 }
-function enviarCorreo(){
-    const email = document.getElementById("email").value;
+
+function enviarCorreo() {
     const usuario = document.getElementById("nombreUsuario").value;
     const apellido = document.getElementById("apellidoUsuario").value;
     const mensaje = document.getElementById("mensajeUsuario").value;
-    console.log(email,usuario,apellido,mensaje);
-    enviarMail(email,usuario,apellido,mensaje);
+    console.log(usuario, apellido, mensaje);
+    enviarMail(usuario, apellido, mensaje);
     return false;
 }
-/* enviarMail('prueba@pruebafacu.com', 'Facu', 'Romero', 'Hola curso, es una prueba'); */
+
